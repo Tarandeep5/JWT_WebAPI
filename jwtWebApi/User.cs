9eebdd22aa32bd16a11a37fb91182f2d64f10f27
Tarandeep5 <tarandeepsingh7837420@gmail.com>
2022-08-25 13:52:33 +0530

    jwt Authentication with .Net 6
    NET 6 Web API  Create JSON Web Tokens (JWT) - User Registration / Login / Authentication

---
+++ jwtWebApi/User.cs
@@ -0,0 +1,10 @@
+﻿namespace jwtWebApi
+{
+    public class User
+    {
+
+        public String Username { get; set; } = String.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }    
+    }
+}
---
+++ jwtWebApi/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace jwtWebApi
+{
+    public class UserDto
+
+    {
+        public String Username { get; set; } = String.Empty;
+        public String Password { get; set; } = String.Empty;    
+
+    }
+}
